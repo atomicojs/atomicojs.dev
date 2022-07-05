b4ec5b7d674692b5e760c4fce2a10d7ded2ee4aa
UpperCod <uppercod.git@gmail.com>
2022-07-05 12:35:52 -0400

    update sections

---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const socials = {
+  twitter: "https://twitter.com/atomicojs",
+  documentation: "https://atomico.gitbook.io/doc/",
+  youtube: "https://www.youtube.com/channel/UCDuCiVASTng8r4jN6BkuNVg",
+  github: "https://github.com/atomicojs/atomico/",
+  koFi: "https://ko-fi.com/atomicojs",
+  discord: "https://t.co/tI70k1DhoN",
+};
+
+export const author = {
+  link: "https://twitter.com/uppercod",
+};
+
+export const menuHeader = [
+  {
+    title: "Github",
+    link: socials.github,
+  },
+  {
+    title: "Documentation",
+    link: socials.documentation,
+  },
+  {
+    title: "Support",
+    link: socials.discord,
+  },
+];
+
+export const menuModal = [
+  {
+    title: "SOCIAL",
+    items: [
+      {
+        title: "Github",
+        href: socials.github,
+      },
+      {
+        title: "Twitter",
+        href: socials.twitter,
+      },
+      {
+        title: "Discord",
+        href: socials.discord,
+      },
+      {
+        title: "ko-fi",
+        href: socials.koFi,
+      },
+      {
+        title: "Youtube",
+        href: socials.youtube,
+      },
+    ],
+  },
+  {
+    title: "STARTED",
+    items: [
+      { title: "Get started", href: "#" },
+      { title: "Template", href: "#" },
+      { title: "Props", href: "#" },
+      { title: "Hooks", href: "#" },
+    ],
+  },
+  {
+    title: "UTILITIES",
+    items: [
+      { title: "@atomico/exports", href: "#" },
+      { title: "@atomico/hooks", href: "#" },
+      { title: "@atomico/ui", href: "#" },
+      { title: "More", href: "#" },
+    ],
+  },
+];
