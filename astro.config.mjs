883aa26f92403bb674b9cb389113c4fc1273a16a
UpperCod <uppercod.git@gmail.com>
2022-08-11 00:12:18 -0400

    add "trust"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import Atomico from "@atomico/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: true,
+    },
+  },
   integrations: [Atomico({ cssLiterals: { minify: true, postcss: true } })],
 });
